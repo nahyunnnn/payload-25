3ba4824fbbc1965581d819ef5042f15b5d4a5328
nahyunnnn <104256766+nahyunnnn@users.noreply.github.com>
2025-04-22 23:49:46 +1000

    Update 1_Simulation.py

--- github_validation/pages/1_Simulation.py
+++ github_validation/pages/1_Simulation.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(layout='wide', page_title = 'Simulation of Payload Tetra')
+st.title('Simulation of Payload Tetra')
+
 def run():
     iframe_src = "https://www.desmos.com/3d/j2bhvgzpr6"
     components.iframe(iframe_src, height = 450)
